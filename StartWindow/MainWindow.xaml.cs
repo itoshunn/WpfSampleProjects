067beee230ae1f668a141e6215249221cae68bfe
itohsunn <tyuke.net@gmail.com>
2016-10-10 18:31:56 +0900

    fast commit

---
+++ StartWindow/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿//--------------------------------------------
+//  StartUp Window
+//--------------------------------------------
+using System.Windows;
+
+namespace StartWindow
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region MinimumProjectの起動
+
+        /// <summary>
+        /// MinimumProjectの起動
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void minimunProject_Start(object sender, RoutedEventArgs e)
+        {
+            MinimumProjects.MainWindow window = new MinimumProjects.MainWindow();
+            window.ShowDialog();
+        }
+
+        #endregion
+
+    }
+}
