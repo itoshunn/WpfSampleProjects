0c19087563def02934fe6c6ef8361f400132235f
itohsunn <tyuke.net@gmail.com>
2016-10-10 18:31:56 +0900

    fast commit

---
+++ EssentialWpf/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace EssentialWpf
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// コントロールウィンドウの表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fastControl_start(object sender, RoutedEventArgs e)
+        {
+            fast.fastControl window = new fast.fastControl();
+            window.ShowDialog();
+        }
+
+        /// <summary>
+        /// 3Dコントロールウィンドウの表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void control3d_Start(object sender, RoutedEventArgs e)
+        {
+            fast.Control3d window = new fast.Control3d();
+            window.ShowDialog();
+            
+        }
+    }
+}
---
+++ EssentialWpf/fast/fastControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------
+// エッセンシャル：ＷＰＦ
+// 第１章 はじめに
+//
+// コントロールの表示サンプル
+//------------------------------------
+using System.Windows;
+
+namespace EssentialWpf.fast
+{
+    /// <summary>
+    /// fastControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class fastControl : Window
+    {
+        public fastControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
